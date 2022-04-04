aad39b129bd4c1e123a8483722edf96c6fbbbc11
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-04 05:57:54 +0500

    jwt means jsonwebtoken

--- server/keys/index.js
+++ server/keys/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGO_URI: "mongodb+srv://Fayzullo:8LPKO6GyPPgZfc8r@cluster0.mkmr1.mongodb.net/Fayzullo_Aiogram"
+    MONGO_URI: "mongodb+srv://Fayzullo:8LPKO6GyPPgZfc8r@cluster0.mkmr1.mongodb.net/Fayzullo_Aiogram",
+    JWT_SECRET: "fayzulloh"
 }
