38a702787479b030b5b03c6bdde3b5e56a81cc88
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-06 20:51:02 +0500

    SignIn and SignUp Frontend

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function SignIn() {
     return(
         <div className="mycard">
-            <h3>Fayzullo Aiogram</h3>
-            <div class="input-field col s6">
-                <i class="material-icons prefix">account_circle</i>
-                <input id="icon_prefix" type="text" class="validate"/>
-                <label for="icon_prefix">First Name</label>
+            <div className="card card__auth">
+                <h2>Fayzullo Aiogram</h2>
+                <div class="input-field col s6">
+                    <i class="material-icons prefix">email</i>
+                    <input id="icon_prefix" type="text" class="validate"/>
+                    <label for="icon_prefix">Pochta manzilingizni</label>
+                </div>
+                <div class="input-field col s6">
+                    <i class="material-icons prefix">password</i>
+                    <input id="icon_prefix" type="text" class="validate"/>
+                    <label for="icon_prefix">Parolingiz</label>
+                </div>
+                <button className="waves-effect waves-light btn #0d471a blue darken-4">Kirish</button>
+                <p>
+                    <Link to="/signUp">Do not you have an account?</Link>
+                </p>
             </div>
         </div>
     )
--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
     return(
-        <h1>SignUp</h1>
+        <div className="mycard">
+            <div className="card card__auth">
+                <h2>Fayzullo Aiogram</h2>
+                <div class="input-field col s6">
+                    <i class="material-icons prefix">verified_user</i>
+                    <input id="icon_prefix" type="text" class="validate"/>
+                    <label for="icon_prefix">Ismingiz</label>
+                </div>
+                <div class="input-field col s6">
+                    <i class="material-icons prefix">email</i>
+                    <input id="icon_prefix" type="text" class="validate"/>
+                    <label for="icon_prefix">Pochta manzilingizni</label>
+                </div>
+                <div class="input-field col s6">
+                    <i class="material-icons prefix">password</i>
+                    <input id="icon_prefix" type="text" class="validate"/>
+                    <label for="icon_prefix">Parolingiz</label>
+                </div>
+                <button className="waves-effect waves-light btn #0d471a blue darken-4">Ro'yhatdan o'tish</button>
+                <p>
+                    <Link to="/signIn">Already have an account?</Link>
+                </p>
+            </div>
+        </div>
     )
 }
