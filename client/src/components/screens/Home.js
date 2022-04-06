d1032990fe52f7e8ac51250680e2af60f5c964d8
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-06 21:27:47 +0500

    Home posts shows Frontend

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,5 +1,73 @@
 export default function Home() {
     return(
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home__card">
+                <h4>Fayzullo</h4>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>It is my first post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+
+            <div className="card home__card">
+                <h4>Fayzullo</h4>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>It is my first post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+
+            <div className="card home__card">
+                <h4>Fayzullo</h4>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>It is my first post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+
+            <div className="card home__card">
+                <h4>Fayzullo</h4>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>It is my first post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+
+            <div className="card home__card">
+                <h4>Fayzullo</h4>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color: "red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>It is my first post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+
+            
+
+        </div>
     )
 }
