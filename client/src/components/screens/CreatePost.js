fc1c226291596adb79e9de4b6ea17a96f7978e30
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-07 15:26:31 +0500

    Maqola qoshish Frontend

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -1,19 +1,33 @@
 export default function CreatePost() {
-    return (
-        <div className="card input-field postCard">
-            <h2>Maqola</h2>
-            <input type="text" placeholder="title" />
-            <input type="text" placeholder="body" />
+  return (
+    <div className="card cardPost">
+      <div className="card-image">
+        <img src="https://images.unsplash.com/photo-1575980861964-fab2c3a7cc24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGFrZSUyMHBob3RvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt='imgphoto'/>
+        <span className="card-title">Rasm joylang</span>
+        <a href='#' className="btn-floating halfway-fab waves-effect waves-light red">
             <div class="file-field input-field">
-            <div className="btn">
-                <span>File</span>
-                <input type="file" />
+                <div className="center pb-2">
+                    <span><i className="material-icons">add</i></span>
+                    <input type="file"/>
+                </div>
+                <div className="file-path-wrapper">
+                    <input className="file-path validate" type="text"/>
+                </div>
             </div>
-            <div className="file-path-wrapper">
-                <input className="file-path validate" type="text" />
-            </div>
-            </div>
-            <button className="waves-effect waves-light btn">Maqola qo'shish</button>
+        </a>
+      </div>
+      <div className="card-content">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">subtitles</i>
+          <input id="icon_prefix" type="text" class="validate" />
+          <label for="icon_prefix">Sarlavha</label>
         </div>
-    )
+        <div class="input-field col s6">
+          <i class="material-icons prefix">content_paste</i>
+          <input id="icon_prefix" type="text" class="validate"/>
+          <label for="icon_prefix">Maqola</label>
+        </div>
+      </div>
+    </div>
+  );
 }
