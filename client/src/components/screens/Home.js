a9c2be8fc13e8eae892698a4ca6dd09dade57e0a
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-09 21:15:09 +0500

    like and unlike API Frontend

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./css/Home.css";
 import HomeSideBar from "./HomeSideBar";
+import {UserContext} from "../../App";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const {state} = useContext(UserContext);
+
   useEffect(() => {
     fetch("http://localhost:5001/allpost", {
       headers: {
@@ -18,6 +21,55 @@ export default function Home() {
         console.log(err);
       });
   }, []);
+
+  const likePost = (id) => {
+    fetch("http://localhost:5001/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Fayzullo " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id
+      })
+    }).then((res) => res.json()).then((result) => {
+      const newData = data.map((item) => {
+        if(item._id === result._id) {
+          return result;
+        } else {
+          return item;
+        }
+      });
+      setData(newData);
+      console.log(result);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  const unlikePost = (id) => {
+    fetch("http://localhost:5001/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Fayzullo " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id
+      })
+    }).then((res) => res.json()).then((result) => {
+      const newData = data.map((item) => {
+        if(item._id === result._id) {
+          return result;
+        } else {
+          return item;
+        }
+      });
+      setData(newData);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
   return (
     <div className="home">
       <div className="home">
@@ -28,15 +80,30 @@ export default function Home() {
                     <div className="card home__card" key={item._id}>
                         <h4 className="postedBy">{item.postedBy.name}</h4>
                         <div className="card-image">
-                            <a href={item.photo} target="_blank"><img
+                            <a href={item.photo}><img
                             src={item.photo}
                             alt={item._id}
                             /></a>
                         </div>
                         <div className="card-content">
-                            <i className="material-icons" style={{ color: "red" }}>
+                          {item.likes.includes(state._id) ? (
+                            <i onClick={() => {
+                              unlikePost(item._id);
+                            }} className="material-icons like"  style={{ color: "red" }}>
                             favorite
                             </i>
+
+                          ):(
+                            <i onClick={() => {
+                              likePost(item._id)
+                            }} className="material-icons like">
+                            favorite
+                            </i>
+
+                          )}
+                        
+                          
+                            <p>{item.likes.length}</p>
                             <h4>{item.title}</h4>
                             <p>{item.body}</p>
                             <input type="text" placeholder="add a comment" />
