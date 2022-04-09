84b8bf8fc9aeb66c80d9bd8cac37211a5afb214c
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-09 12:32:18 +0500

    like and unlike API Backend

--- server/models/post.js
+++ server/models/post.js
@@ -14,6 +14,12 @@ const postSchema = new Schema({
         type: String,
         required: true,
     },
+    likes: [
+        {
+            type: ObjectId,
+            ref: "User"
+        }
+    ],
     postedBy: {
         type: ObjectId,
         ref: "User"
