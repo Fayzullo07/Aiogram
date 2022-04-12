c20ec5e80e99d83ab328e2d0f8bebddfd7ff321c
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-04-12 21:56:11 +0500

    Subscribtions Backend API

--- client/src/reducers/useReducer.js
+++ client/src/reducers/useReducer.js
@@ -2,7 +2,7 @@ export const initialState = null;
 
 export const reducer = (state, action) => {
     if(action.type === "USER") {
-        return {...action.payload, following: [], followers: []};
+        return {...action.payload};
     }
     if(action.type === "CLEAR") {
         return null;
